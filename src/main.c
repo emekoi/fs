d3eb692428c0cd026e49a4ef28e5fe94c25f81c8
emekoi <emekankurumeh@outlook.com>
2017-09-03 23:07:42 -0500

    added example

--- src/main.c
+++ src/main.c
@@ -3,9 +3,18 @@
 #include "fs.h"
 
 int main(int argc, const char **argv) {
-  puts("Hello World");
   ((void) argc);
   ((void) argv);
-  /* code */
+
+  fs_error(fs_mount(argv[1])) ;
+
+  fs_FileListNode *list = fs_listDir(".");
+  fs_FileListNode *n = list;
+  while (n) {
+    puts(n->name);
+    n = n->next;
+  }
+  fs_freeFileList(list);
+  fs_deinit();
   return 0;
 }
