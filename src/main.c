abca44c36a0cb1e318f9ae2037f69c4e338df718
emekoi <emekankurumeh@outlook.com>
2017-09-03 23:10:43 -0500

    added example

--- src/main.c
+++ src/main.c
@@ -8,10 +8,17 @@ int main(int argc, const char **argv) {
 
   fs_error(fs_mount(argv[1])) ;
 
+  /*
+  http://www.theasciicode.com.ar/extended-ascii-code/angle-quotes-guillemets-right-pointing-double-angle-french-quotation-marks-ascii-code-174.html
+  └
+  ├
+  */
+
   fs_FileListNode *list = fs_listDir(".");
   fs_FileListNode *n = list;
   while (n) {
-    puts(n->name);
+    printf("├ %s\n", n->name);
+    if
     n = n->next;
   }
   fs_freeFileList(list);
